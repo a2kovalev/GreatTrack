cb4f3c251d512cd848e604cfb04fbe6b3335a0f2
Alexander Kovalev <alexanderkovalev@Alexanders-MacBook-Pro-2.local>
2020-07-28 10:09:09 -0400

    Added emojis to the cards

--- app/src/main/java/com/example/greattrack/mood/MoodFragment.java
+++ app/src/main/java/com/example/greattrack/mood/MoodFragment.java
@@ -17,6 +17,8 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.greattrack.R;
+import com.vdurmont.emoji.Emoji;
+import com.vdurmont.emoji.EmojiManager;
 
 import org.w3c.dom.Text;
 
@@ -34,13 +36,52 @@ public class MoodFragment extends Fragment {
             Log.d("MTAG", "adding mood: " + moodType.name());
             CardView cardView = new CardView(this.getActivity());
             LinearLayout.LayoutParams cardViewParams =
-                    new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 200);
             TextView textView = new TextView(this.getActivity());
             LinearLayout.LayoutParams textViewParams =
-                    new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            textView.setText(moodType.toString());
+                    new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            String emotionText = "";
+            Emoji emoji;
+            switch (moodType) {
+                case HAPPY:
+                    emoji = EmojiManager.getForAlias("smile");
+                    emotionText = emoji.getUnicode() + " Happy";
+                    break;
+                case CONTENT:
+                    emoji = EmojiManager.getForAlias("relieved");
+                    emotionText = emoji.getUnicode() + " Content";
+                    break;
+                case NEUTRAL:
+                    emoji = EmojiManager.getForAlias("neutral_face");
+                    emotionText = emoji.getUnicode() + " Neutral";
+                    break;
+                case SAD:
+                    emoji = EmojiManager.getForAlias("pensive");
+                    emotionText = emoji.getUnicode() + " Sad";
+                    break;
+                case AWFUL:
+                    emoji = EmojiManager.getForAlias("weary");
+                    emotionText = emoji.getUnicode() + " Awful";
+                    break;
+                case ANGRY:
+                    emoji = EmojiManager.getForAlias("rage");
+                    emotionText = emoji.getUnicode() + " Angry";
+                    break;
+                case TIRED:
+                    emoji = EmojiManager.getForAlias("sleeping");
+                    emotionText = emoji.getUnicode() +  " Tired";
+                    break;
+                case BORED:
+                    emoji = EmojiManager.getForAlias("unamused");
+                    emotionText = emoji.getUnicode() + " Bored";
+                    break;
+            }
+            textView.setText(emotionText);
+            cardView.setCardElevation(0);
             textView.setTextSize(25);
             textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            textView.setGravity(Gravity.CENTER_VERTICAL);
+            cardView.setRadius(20);
             cardViewParams.setMargins(200, 30, 200, 0);
             cardView.addView(textView, textViewParams);
             linearLayout.addView(cardView, cardViewParams);
