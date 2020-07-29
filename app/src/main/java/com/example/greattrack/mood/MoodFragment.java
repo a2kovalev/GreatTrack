214f5bd626e178635eee7b59106b96ddae65715c
Alexander Kovalev <alexanderkovalev@Alexanders-MacBook-Pro-2.local>
2020-07-29 12:18:24 -0400

    Adding functionality to mood

--- app/src/main/java/com/example/greattrack/mood/MoodFragment.java
+++ app/src/main/java/com/example/greattrack/mood/MoodFragment.java
@@ -1,5 +1,7 @@
 package com.example.greattrack.mood;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,25 +9,26 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-
 import com.example.greattrack.R;
 import com.vdurmont.emoji.Emoji;
 import com.vdurmont.emoji.EmojiManager;
 
-import org.w3c.dom.Text;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class MoodFragment extends Fragment {
 
     private static final String TAG = "Mood Fragment";
     LinearLayout linearLayout;
+    ArrayList<Mood> moodList = new ArrayList<>();
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -36,8 +39,9 @@ public class MoodFragment extends Fragment {
             Log.d("MTAG", "adding mood: " + moodType.name());
             CardView cardView = new CardView(this.getActivity());
             LinearLayout.LayoutParams cardViewParams =
-                    new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 200);
+                    new LinearLayout.LayoutParams(700, 200);
             TextView textView = new TextView(this.getActivity());
+            textView.setTextColor(Color.parseColor("#f3f3f3"));
             LinearLayout.LayoutParams textViewParams =
                     new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
             String emotionText = "";
@@ -46,34 +50,42 @@ public class MoodFragment extends Fragment {
                 case HAPPY:
                     emoji = EmojiManager.getForAlias("smile");
                     emotionText = emoji.getUnicode() + " Happy";
+                    cardView.setCardBackgroundColor(Color.parseColor("#0080ff"));
                     break;
                 case CONTENT:
                     emoji = EmojiManager.getForAlias("relieved");
                     emotionText = emoji.getUnicode() + " Content";
+                    cardView.setCardBackgroundColor(Color.parseColor("#18a8a3"));
                     break;
                 case NEUTRAL:
                     emoji = EmojiManager.getForAlias("neutral_face");
                     emotionText = emoji.getUnicode() + " Neutral";
+                    cardView.setCardBackgroundColor(Color.parseColor("#45bd00"));
                     break;
                 case SAD:
                     emoji = EmojiManager.getForAlias("pensive");
                     emotionText = emoji.getUnicode() + " Sad";
+                    cardView.setCardBackgroundColor(Color.parseColor("#97a818"));
                     break;
                 case AWFUL:
                     emoji = EmojiManager.getForAlias("weary");
                     emotionText = emoji.getUnicode() + " Awful";
+                    cardView.setCardBackgroundColor(Color.parseColor("#a88218"));
                     break;
                 case ANGRY:
                     emoji = EmojiManager.getForAlias("rage");
                     emotionText = emoji.getUnicode() + " Angry";
+                    cardView.setCardBackgroundColor(Color.parseColor("#c93204"));
                     break;
                 case TIRED:
                     emoji = EmojiManager.getForAlias("sleeping");
                     emotionText = emoji.getUnicode() +  " Tired";
+                    cardView.setCardBackgroundColor(Color.parseColor("#7a04cf"));
                     break;
                 case BORED:
                     emoji = EmojiManager.getForAlias("unamused");
                     emotionText = emoji.getUnicode() + " Bored";
+                    cardView.setCardBackgroundColor(Color.parseColor("#a3a3a3"));
                     break;
             }
             textView.setText(emotionText);
@@ -82,8 +94,40 @@ public class MoodFragment extends Fragment {
             textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
             textView.setGravity(Gravity.CENTER_VERTICAL);
             cardView.setRadius(20);
-            cardViewParams.setMargins(200, 30, 200, 0);
+            cardViewParams.gravity = Gravity.CENTER_HORIZONTAL;
+            cardViewParams.setMargins(20, 15, 20, 15);
             cardView.addView(textView, textViewParams);
+
+            cardView.setOnClickListener(v -> {
+                View alertView = getLayoutInflater().inflate(R.layout.add_mood_alert, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(this.getContext());
+                builder.setView(alertView);
+                builder.setTitle("You selected \"" + moodType.name() + "\"");
+                builder.setMessage("If you want, add a short description below! Max. 160 chars");
+                EditText moodDescription = (EditText) alertView.findViewById(R.id.addMoodEditText);
+                builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        LocalDate currDate = LocalDate.now();
+                        MoodDate moodDate = new MoodDate(currDate.getDayOfMonth(), currDate.getMonthValue() + 1, currDate.getYear());
+                        String moodExplanation = moodDescription.getText().toString();
+                        Mood newMood = new Mood(moodType, moodDate, moodExplanation);
+                        Log.d("MTAG", "Adding mood with explanation: " + newMood.explanation);
+                        moodList.add(newMood);
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            });
+
             linearLayout.addView(cardView, cardViewParams);
         }
 
